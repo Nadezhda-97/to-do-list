03d91c8bfebcc6526003d37ce88bb5c016dfe0b7
nnadya <n.d.niki2012@gmail.com>
2025-04-03 22:50:23 +0300

    update TodoItem.tsx

--- src/app/components/TodoItem.tsx
+++ src/app/components/TodoItem.tsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import TodoItemEdit from "./TodoItemEdit";
 import Todo from "../types/Todo";
 
 interface TodoItemProps {
   todo: Todo;
   onToggleComplete: (id: number) => void;
-  onEdit: (id: number, content: string) => void;
+  onUpdate: (id: number, content: string) => void;
   onDelete: (id: number) => void;
+  index: number;
 }
 
-const TodoItem: React.FC<TodoItemProps> = ({ todo, onToggleComplete, onEdit, onDelete }) => {
+const TodoItem: React.FC<TodoItemProps> = ({ todo, onToggleComplete, onUpdate, onDelete, index }) => {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  const handleUpdate = (content: string) => {
+    onUpdate(todo.id,content);
+    setIsEditing(false);
+  };
+
   return (
     <li style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-      {todo.content}
-      <button onClick={() => onToggleComplete(todo.id)}>
-        {todo.completed ? 'Undo' : 'Complete'}
-      </button>
-      <button onClick={() => onEdit(todo.id, todo.content)}>Edit</button>
-      <button onClick={() => onDelete(todo.id)}>Delete</button>
+      {isEditing ? (
+        <TodoItemEdit
+          initialValue={todo.content}
+          onSave={handleUpdate}
+          onCancel={() => setIsEditing(false)}
+        />
+      ) : (
+        <div>
+          <span>{index}. {todo.content}</span>
+          <button onClick={() => onToggleComplete(todo.id)}>
+            {todo.completed ? 'Undo' : 'Complete'}
+          </button>
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+          <button onClick={() => onDelete(todo.id)}>Delete</button>
+        </div>
+      )}
     </li>
   )
 };
