8624d7b8d1ddb25587918d7b233834d9ba710c91
nnadya <n.d.niki2012@gmail.com>
2025-04-07 22:13:25 +0300

    update validation.ts

--- src/app/utils/validation.ts
+++ src/app/utils/validation.ts
@@ -1,6 +1,10 @@
+'use client'
+
+import i18n from '../i18n';
+
 export const validateTask = (task: string, maxLength: number): string | null => {
-  if (task.trim() === '') return 'Текст задачи не может быть пустым';
-  if (task.length < 3) return 'Минимальная длина задачи - 3 символа';
-  if (task.length > maxLength) return `Максимальная длина задачи - ${maxLength} символов`;
+  if (task.trim() === '') return i18n.t('errorRequired')
+  if (task.length < 3) return i18n.t('errorTooShort');
+  if (task.length > maxLength) return i18n.t('errorTooLong', { max: maxLength });
   return null;
 };
