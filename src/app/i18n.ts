19e19551727d1bec905b07246f7c47e25072bd79
nnadya <n.d.niki2012@gmail.com>
2025-04-10 22:37:38 +0300

    update i18n.ts

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -3,6 +3,9 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import translationRu from './locales/ru/translation.json';
+import translationEn from './locales/en/translation.json';
+import translationDe from './locales/de/translation.json';
 
 i18n
   .use(LanguageDetector)
@@ -14,51 +17,9 @@ i18n
       escapeValue: false,
     },
     resources: {
-      ru: {
-        translation: {
-          title: 'Список задач',
-          placeholder: 'Введите описание задачи',
-          addTask: 'Добавить задачу',
-          edit: 'Редактировать',
-          delete: 'Удалить',
-          save: 'Сохранить',
-          cancel: 'Отмена',
-          errorRequired: 'Текст задачи не может быть пустым',
-          errorTooShort: 'Минимальная длина задачи - 3 символа',
-          errorTooLong: 'Максимальная длина задачи - {{max}} символов',
-
-          markAsCompleted: "Отметить как выполненное",
-          markAsIncomplete: "Снять отметку о выполнении",
-        },
-      },
-      en: {
-        translation: {
-          title: 'ToDo List',
-          placeholder: 'Add task description',
-          addTask: 'Add Task',
-          edit: 'Edit',
-          delete: 'Delete',
-          save: 'Save',
-          cancel: 'Cancel',
-          errorRequired: 'Task cannot be empty',
-          errorTooShort: 'Task must be at least 3 characters long',
-          errorTooLong: 'Task must not exceed {{max}} characters',
-        },
-      },
-      de: {
-        translation: {
-          title: 'Aufgabenliste',
-          placeholder: 'Aufgabenbeschreibung hinzufügen',
-          addTask: 'Aufgabe hinzufügen',
-          edit: 'Bearbeiten',
-          delete: 'Löschen',
-          save: 'Speichern',
-          cancel: 'Abbrechen',
-          errorRequired: 'Die Aufgabe darf nicht leer sein',
-          errorTooShort: 'Die Aufgabe muss mindestens 3 Zeichen lang sein',
-          errorTooLong: 'Die Aufgabe darf {{max}} Zeichen nicht überschreiten',
-        },
-      },
+      ru: { translation: translationRu },
+      en: { translation: translationEn },
+      de: { translation: translationDe },
     },
   });
 
