b4fb6ba89059d6c2b36abe508009f8dd463ee101
nnadya <n.d.niki2012@gmail.com>
2025-04-16 22:23:25 +0300

    update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,8 +3,9 @@ import { getServerTranslation } from '@/i18n/server';
 import TodoList from '../components/TodoList';
 
 export default async function Home({ params: { locale } }: { params: { locale: string } }) {
-  const i18n = await getServerTranslation(locale);  // получаем серверный экземпляр i18next
-  const t = i18n.t;
+  const i18n = await getServerTranslation(locale);
+  //const t = i18n.t;
+  const translations = i18n.getDataByLanguage(locale)?.translation || {};
 
-  return <TodoList t={t} />;
+  return <TodoList locale={locale} translations={translations} />;
 }
