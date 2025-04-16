f129b4f5e6b0c26c92a92a6c300fc4529e19fb15
nnadya <n.d.niki2012@gmail.com>
2025-04-16 23:47:38 +0300

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import { languages, fallbackLng } from "@/i18n/settings";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  /* i18n: {
+    locales: languages,
+    defaultLocale: fallbackLng,
+  }, */
 };
 
 export default nextConfig;
