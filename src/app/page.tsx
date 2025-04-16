65291ad78c5f530458587ddfddb8ebafb9bdfddc
nnadya <n.d.niki2012@gmail.com>
2025-04-16 23:49:21 +0300

    update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import './styles/index.css';
-import TodoList from "./components/TodoList";
-import './i18n';
+import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
+import { languages, fallbackLng } from '@/i18n/settings';
 
-export default function Home() {
-  return (
-    <div>
-      <TodoList />
-    </div>
-  );
+export default function RootPage() {
+  const headersList = headers();
+  const acceptLanguage = headersList.get('accept-language');
+  const preferredLang = acceptLanguage?.split(',')[0].split('-')[0];
+
+  const matchedLocale = languages.includes(preferredLang || '') ? preferredLang : fallbackLng;
+
+  redirect(`/${matchedLocale}`);
 }
