7f39495580a4efe3aca5a40e00d6e8e6f378b9c0
nnadya <n.d.niki2012@gmail.com>
2025-04-17 22:31:24 +0300

    update page.tsx in [locale]

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,21 @@
 import '../styles/index.css';
 import { getServerTranslation } from '@/i18n/server';
 import TodoList from '../components/TodoList';
+import Translations from '../types/Translations';
+import isTranslations from '../utils/isTranslations';
 
-export default async function Home({ params: { locale } }: { params: { locale: string } }) {
+export default async function Home(props: { params: { locale: string } }) {
+  const locale = props.params.locale;
   const i18n = await getServerTranslation(locale);
-  //const t = i18n.t;
-  const translations = i18n.getDataByLanguage(locale)?.translation || {};
+  const rawTranslations = i18n.getDataByLanguage(locale)?.translation || {};
+
+  const translations: Translations = isTranslations(rawTranslations)
+  ? rawTranslations
+  : {
+      errorRequired: 'Missing errorRequired',
+      errorTooShort: 'Missing errorTooShort',
+      errorTooLong: 'Missing errorTooLong',
+    };
 
   return <TodoList locale={locale} translations={translations} />;
 }
