edf57319db3d4cba79286877a2f5082e3476c1f4
nnadya <n.d.niki2012@gmail.com>
2025-04-21 22:43:23 +0300

    add async/await

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { redirect } from 'next/navigation';
 import { headers } from 'next/headers';
 import { languages, fallbackLng } from '@/i18n/settings';
 
-export default function RootPage() {
-  const headersList = headers();
+export default async function RootPage() {
+  const headersList = await headers();
   const acceptLanguage = headersList.get('accept-language');
   const preferredLang = acceptLanguage?.split(',')[0].split('-')[0];
 
