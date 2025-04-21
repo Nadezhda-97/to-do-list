50bc693614ef41464126fb0a123dce75d8471f09
nnadya <n.d.niki2012@gmail.com>
2025-04-21 22:47:59 +0300

    update [locale]/layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,26 +1,53 @@
 import { dir } from 'i18next';
 import { languages } from '@/i18n/settings';
 
-interface LocaleLayoutProps {
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'ToDo App',
+  description: 'Multilingual ToDo App built with Next.js 13+',
+};
+
+/* interface LocaleLayoutProps {
   children: React.ReactNode;
   params: {
     locale: string;
   };
-}
+} */
 
 export async function generateStaticParams() {
   return languages.map((lng) => ({ locale: lng }));
 }
 
-export default async function LocaleLayout(props: LocaleLayoutProps) {
+/* export default async function LocaleLayout(props: LocaleLayoutProps) {
   const locale = props.params.locale;
+  const direction = dir(locale);
+
+  return (
+    <div lang={locale} dir={direction}>
+      {props.children}
+    </div>
+  );
+} */
+
+/* type LayoutProps = {
+  children: React.ReactNode;
+  params: {
+    locale: string;
+  };
+}; */
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function LocaleLayout(props: any) {
+  const { children, params } = props;
+  const locale = params.locale;
   const direction = dir(locale);
 
   return (
     <html lang={locale} dir={direction}>
       <body>
-        {props.children}
+        {children}
       </body>
     </html>
-  );
+  )
 }
