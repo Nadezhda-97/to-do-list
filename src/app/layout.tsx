f3729564560c5b6151ff5fcb10f25170ab15eb0c
nnadya <n.d.niki2012@gmail.com>
2025-04-21 22:47:11 +0300

    update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
-//import '../globals.css';
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'ToDo App',
-  description: 'Multilingual ToDo App built with Next.js 13+',
-};
-
-export default function RootLayout({ children, params }: { children: React.ReactNode; params: { locale: string } }) {
-  const locale = params.locale;
-  return (
-    <html lang={locale}>
-      <body>{children}</body>
-    </html>
-  );
-}
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return children;
+  }
+  
