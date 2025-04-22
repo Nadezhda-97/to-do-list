af382f3dfd80887c7a26f49cefb9097feb89a06f
nnadya <n.d.niki2012@gmail.com>
2025-04-22 22:43:23 +0300

    delete comments

--- src/app/utils/validation.ts
+++ src/app/utils/validation.ts
@@ -1,11 +1,10 @@
 'use client'
 
-//import i18n from '../i18n';
 import Translations from "../types/Translations";
 
 export const validateTask = (task: string, maxLength: number, translations: Translations): string | null => {
-  if (task.trim() === '') return translations.errorRequired; /* t('errorRequired'); */
-  if (task.length < 3) return translations.errorTooShort; /* t('errorTooShort'); */
-  if (task.length > maxLength) return translations.errorTooLong.replace('{{max}}', String(maxLength)); /* t('errorTooLong', { max: maxLength }); */
+  if (task.trim() === '') return translations.errorRequired;
+  if (task.length < 3) return translations.errorTooShort;
+  if (task.length > maxLength) return translations.errorTooLong.replace('{{max}}', String(maxLength));
   return null;
 };
