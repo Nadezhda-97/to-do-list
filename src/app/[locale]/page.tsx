1c0f746bd62ec700b18037d180f67bd2960b5189
nnadya <n.d.niki2012@gmail.com>
2025-04-22 22:46:17 +0300

    delete comment

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -11,7 +11,7 @@ import isTranslations from '../utils/isTranslations';
 } */
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default async function Home(props: any) { /* props: { params: { locale: string } } */
+export default async function Home(props: any) {
   const locale = props.params.locale;
   const i18n = await getServerTranslation(locale);
   const rawTranslations = i18n.getDataByLanguage(locale)?.translation || {};
