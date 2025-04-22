35f8a7f97b37ce2cab538c930a2fb532f720bf8d
nnadya <n.d.niki2012@gmail.com>
2025-04-22 22:41:51 +0300

    delete comments

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
-//import { languages, fallbackLng } from "@/i18n/settings";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  /* i18n: {
-    locales: languages,
-    defaultLocale: fallbackLng,
-  }, */
 };
 
 export default nextConfig;
