79e71ed184e38a3e9245a13156a91630e8107459
nnadya <n.d.niki2012@gmail.com>
2025-04-22 22:45:38 +0300

    delete comments

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,27 +8,9 @@ export const metadata: Metadata = {
   description: 'Multilingual ToDo App built with Next.js 13+',
 };
 
-/* interface LocaleLayoutProps {
-  children: React.ReactNode;
-  params: {
-    locale: string;
-  };
-} */
-
 export async function generateStaticParams() {
   return languages.map((lng) => ({ locale: lng }));
 }
-
-/* export default async function LocaleLayout(props: LocaleLayoutProps) {
-  const locale = props.params.locale;
-  const direction = dir(locale);
-
-  return (
-    <div lang={locale} dir={direction}>
-      {props.children}
-    </div>
-  );
-} */
 
 /* type LayoutProps = {
   children: React.ReactNode;
